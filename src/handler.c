d4ff1c6ebcdf07293f9538b5c05e5f35721dd647
Artem (Drunkslime) Babichenko <tuturu352@gmail.com>
2025-03-19 23:10:40 +0000

    Added handler

---
+++ src/handler.c
@@ -0,0 +1,75 @@
+#include <microhttpd.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "include/utils.h"
+
+#define DEFPAGE "<html><body><form action=\'/hello\'><button type=\'submit\' method=\'POST\'>Click me</button></form></body></html>"
+#define PAGE(s) "<html><body><h1>" s "</h1></body></html>"
+
+static enum MHD_Result 
+ahc_echo(
+    void * cls,
+    struct MHD_Connection * connection,
+    const char * url,
+    const char * method,
+    const char * version,
+    const char * upload_data, 
+    size_t * upload_data_size, 
+    void ** ptr) {
+        
+        char * url_str = (char * ) url;
+        char * method_str = (char * ) method;
+        int ret;
+
+        struct MHD_Response * response;
+        HTTP_Response http_response;
+
+        if (strcmp(url_str, "/") == 0) {
+            http_response = (HTTP_Response){
+                .body = DEFPAGE,
+                .status = OK
+            };
+        } else if (strcmp(url_str, "/hello") == 0) {
+            if (strcmp(method_str, "POST") == 0) {
+                http_response = (HTTP_Response){
+                    .body = ("<script>console.log('Fuck You Bastard, You called", method_str, " method')</script>"),
+                    .status = OK
+                };
+            } else {
+                http_response = (HTTP_Response){
+                    .body = PAGE("Hello"),
+                    .status = OK
+                };
+            }
+        } else if (strcmp(url_str, "/world") == 0) {
+            http_response = (HTTP_Response){
+                .body = PAGE("World"),
+                .status = OK
+            };
+        } else {
+            http_response = (HTTP_Response){
+                .body = PAGE("Not Found"),
+                .status = NOT_FOUND
+            };
+        }
+
+        char * page = http_response.body;
+
+        response = MHD_create_response_from_buffer(
+            strlen(page), 
+            (void* ) page, 
+            MHD_RESPMEM_PERSISTENT
+        );
+
+        if (!response) return MHD_NO;
+
+        ret = MHD_queue_response(
+            connection,
+            http_response.status,
+            response
+        );
+
+        MHD_destroy_response(response);
+        return ret;
+    }
---
+++ src/include/utils.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+typedef enum {
+    OK = 200,
+    BAD_REQUEST = 400,
+    NOT_FOUND = 404,
+    INTERNAL_SERVER_ERROR = 500,
+    NOT_IMPLEMENTED = 501
+} HTTP_Status;
+
+typedef struct {
+    char * body;
+    HTTP_Status status;
+} HTTP_Response;
+
+#endif
